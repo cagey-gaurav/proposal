d2880b4993193d6cb7c30265dee950079b157b31
cagey-gaurav <135024177+cagey-gaurav@users.noreply.github.com>
2024-10-22 21:28:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showLoveMessage() {
+    const loveMessage = document.getElementById('love-message');
+    loveMessage.style.display = 'block';
+    loveMessage.classList.add('animate-message');
+}
